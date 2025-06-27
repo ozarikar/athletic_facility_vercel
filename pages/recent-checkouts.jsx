cf69d53e57eb4e7916fac7831857d8e76e6b0fae
ozarikar <omkar.zarikar@centre.edu>
2025-06-27 03:12:01 -0400

    Mobile-first styling, global layout, Home links

--- pages/recent-checkouts.jsx
+++ pages/recent-checkouts.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Layout from '../components/Layout';
 
 export default function RecentCheckouts() {
   const [checkouts, setCheckouts] = useState([]);
